b19130c6fda2bb016b5e33a00a837a46b028f23f
Natalia <lra.natalia@yahoo.com.br>
2025-01-13 16:29:15 -0300

    feat criação navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FibonacciComponent} from "./fibonacci/fibonacci.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'fibonacci', component: FibonacciComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
