7a178048323ab70141f2473245e46374f7094568
Natalia <lra.natalia@yahoo.com.br>
2025-01-13 16:33:38 -0300

    feat importação formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FibonacciComponent } from './fibonacci/fibonacci.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FibonacciComponent } from './fibonacci/fibonacci.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
